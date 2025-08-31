00a7fcb2645a4ca187b978ecaf569689e0608703
ot0620 <sy0320hn0320@gmail.com>
2025-08-31 21:42:23 +0900

    t=0状態を参照した次数順攻撃

--- graph-tool/attack/degree_attack(initial).py
+++ graph-tool/attack/degree_attack(initial).py
@@ -1,9 +1,7 @@
 def initial_degree_attack(g,Node):
-
     vertices = sorted([v for v in g.vertices()], key=lambda v: v.out_degree())
     sizes, comp = gt.vertex_percolation(g, vertices)
     degree_attack = [frc for frc in sizes]
     perc = np.array(degree_attack)
     perc = perc / Node
-    
     return perc
